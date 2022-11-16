4c6def13cde4e390d43d25ab90de989ec308c61e
agneja00 <agnieshaa00@gmail.com>
2022-11-16 11:53:14 +0200

    java

---
+++ index.js
@@ -0,0 +1,10 @@
+// let firstName = "Agnieska";
+// let secondName = "Jackevic";
+// let pilnas = firstName + " " + secondName;
+
+// alert(pilnas);
+
+let x = 123;
+let tipas = typeof x;
+
+console.log(tipas);
